51a762f00536f4caf78df68115649412d7d1b78a
Keegan McRoberts <keegan.mcroberts@gmail.com>
2022-06-06 16:58:52 -0500

    commit tree 3a64d3ef3247e577f034374bd00fadbcb40ebb9d

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
+  let result;
+  if (distance < 400){
+    result = 'This one is on me!'
+  } else if (distance > 400, distance <2000){
+    result = 'That will be twenty bucks.'
+  } else if (distance > 2000, distance <2500){
+    result = 'I will gladly take your thirty bucks.'
+  } else if (distance > 2500){
+    result = "No can do."
+  }
+  return result;
+
   // Write your code here!
 }
+scuberGreetingForFeet(199)
+scuberGreetingForFeet(1500)
+scuberGreetingForFeet(2001)
+scuberGreetingForFeet(2501)
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+  const message = city === "NYC" ? 'Ok, sounds good.' : 'No go.';
+  return message 
 }
+ternaryCheckCity("NYC");
+ternaryCheckCity("Pittsburgh");
 
-function switchOnCharmFromTip(){
+  // Write your code here!
+
+
+function switchOnCharmFromTip(tip){
+  let message;
+  switch (tip) {
+    case 'generous':
+      message = "Thank you so much."
+      break;
+    case 'not as generous':
+      message = "Thank you."
+      break;
+    case 'thanks for everything':
+      message = "Bye."
+      break;
+  }
+  return message 
   // Write your code here!
 }
